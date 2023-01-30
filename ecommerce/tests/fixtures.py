a9d628ba247a0ea95ec6b53e63fe651875ce0e61
Benedict <newlyf38@gmail.com>
2023-01-30 11:26:42 +0000

    done setting up the fixture for testing

--- ecommerce/tests/fixtures.py
+++ ecommerce/tests/fixtures.py
@@ -5,4 +5,4 @@ def create_admin_user(django_user_model):
     """
     Return admin User
     """
-    return django_user_model.objects.create_superuser
+    return django_user_model.objects.create_superuser('admin', 'admin@gmail.com', 'admin')
