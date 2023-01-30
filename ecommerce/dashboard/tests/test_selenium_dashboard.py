2024715c01989e9d8773a646cdcfed434bd0d2e9
Benedict <newlyf38@gmail.com>
2023-01-30 11:09:11 +0000

    done setting up the testing environment but it failed because there was no database connected from the testing to the project database and i am now connecting it using fixtures

--- ecommerce/dashboard/tests/test_selenium_dashboard.py
+++ ecommerce/dashboard/tests/test_selenium_dashboard.py
@@ -1,2 +1,23 @@
-def test_example():
-    assert 'hello' == 'hello'
+# def test_example():
+#     assert 'hello' == 'hello'
+
+import pytest
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+@pytest.mark.selenium
+def test_dashboard_admin_login(live_server, chrome_browser_instance):
+    
+    browser = chrome_browser_instance
+    
+    browser.get(("%s%s" % (live_server.url, "/admin/login/")))
+    
+    user_name = browser.find_element(By.NAME, "username")
+    user_password = browser.find_element(By.NAME, "password")
+    submit = browser.find_element(By.XPATH, '//input[@value="Log in"]')
+    
+    user_name.send_keys('admin')
+    user_password.send_keys('admin')
+    submit.send_keys(Keys.RETURN)
+    
+    assert 'Site administration' in browser.page_source
